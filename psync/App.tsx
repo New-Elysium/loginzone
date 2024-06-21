4784ffd85b9bc35319acd4c6a3cdc4798c9187df
harshit-506 <supriyamohapatra658@gmail.com>
2024-06-21 09:27:25 +0530

    importing LoginScreen in in App.tsx

--- psync/App.tsx
+++ psync/App.tsx
@@ -1,15 +1,29 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import LoginScreen from "./screens/LoginScreen";
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <LoginScreen />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#140034', // Set the background color of the entire app
+  },
+  innerContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%', // Ensure the inner container takes full width
+  },
+});
 
-const styles = StyleSheet.create({})
+export default App;
